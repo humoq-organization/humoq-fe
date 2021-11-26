9bf4bc6a046cbb6d6f2a825ac451512e4ad01f6f
karabatak <oktay.karabatak@gmail.com>
2021-11-26 21:14:31 +0300

    manifest fix

--- components/Meta.js
+++ components/Meta.js
@@ -9,7 +9,7 @@ const Meta = ({title, keywords, description}) => {
             <meta name='keywords' content={keywords} />
             <meta name='description' content={description} />
             <meta charSet='utf-8' />
-            <link rel="manifest" href="/manifest.json" />
+            <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
