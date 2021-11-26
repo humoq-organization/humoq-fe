82018adb22e7e16d8261c389ab863643340f283c
karabatak <oktay.karabatak@gmail.com>
2021-11-27 02:30:18 +0300

    serviceworker remove

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useRouter } from 'next/router';
 import Layout from '../components/Layout'
 import { IntlProvider } from 'react-intl';
@@ -13,25 +12,6 @@ const languages = {
 export default function MyApp({ Component, pageProps }) {
     const router = useRouter();
     const { locale, defaultLocale } = router;
-
-    useEffect(() => {
-        if ("serviceWorker" in navigator) {
-          window.addEventListener("load", function () {
-            navigator.serviceWorker.register("/sw.js").then(
-              function (registration) {
-                console.log(
-                  "Service Worker registration successful with scope: ",
-                  registration.scope
-                );
-              },
-              function (err) {
-                console.log("Service Worker registration failed: ", err);
-              }
-            );
-          });
-        }
-      }, []);
-
     let settedLocale = "en";
     if (typeof window !== 'undefined') {
         if (window.location.origin.includes('humoq.de')) {
