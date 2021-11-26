d169458ecf474b71dde11750886ac7fe56754a62
karabatak <oktay.karabatak@gmail.com>
2021-11-27 02:47:03 +0300

    config fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = withPWA({
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === "development",
+    register: true,
     runtimeCaching,
     buildExcludes: [
         /middleware-manifest\.json$/,
