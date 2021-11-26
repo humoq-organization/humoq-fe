19d6da89cfda784cf6ade742845f0863777bce4e
karabatak <oktay.karabatak@gmail.com>
2021-11-26 20:47:05 +0300

    pwa settings fix

--- next.config.js
+++ next.config.js
@@ -4,8 +4,9 @@ const runtimeCaching = require('next-pwa/cache')
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    runtimeCaching,
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
   },
   i18n: {
     locales: ['en', 'de'],
