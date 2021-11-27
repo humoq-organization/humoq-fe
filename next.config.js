841606da9b103567df543b8b3d8b09f4d8cf29f1
karabatak <oktay.karabatak@gmail.com>
2021-11-27 03:14:18 +0300

    config fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+    buildExcludes: [
+        /middleware-manifest\.json$/,
+        /_middleware.js$/,
+        /_middleware.js.map$/,
+    ],
   },
   i18n: {
     locales: ['en', 'de'],
