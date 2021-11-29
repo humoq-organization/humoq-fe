fbfcd1af8abc1cd386decfcb7ef64e2d48428a32
karabatak <oktay.karabatak@gmail.com>
2021-11-29 22:25:43 +0300

    meta update

--- components/Meta.js
+++ components/Meta.js
@@ -14,7 +14,6 @@ const Meta = ({title, keywords, description}) => {
             <meta name='apple-mobile-web-app-capable' content='yes' />
             <meta name='apple-mobile-web-app-status-bar-style' content='default' />
             <meta name='apple-mobile-web-app-title' content='Humoq' />
-            <meta name='description' content='Play free online games: arcade games, puzzle games, sports games, shooting games, and more.' />
             <meta name='format-detection' content='telephone=no' />
             <meta name='mobile-web-app-capable' content='yes' />
 
