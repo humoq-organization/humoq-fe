b742cfb69206121f8bb29aad7a212ca479a181b2
karabatak <oktay.karabatak@gmail.com>
2021-12-05 19:20:52 +0300

    robots update

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -3,28 +3,12 @@ import React from "react";
 class Robots extends React.Component {
   static async getInitialProps({ req, res }) {
 
-    const domain = req.headers.host;
-    console.log(`request_source ${domain}`);
-  
-    if (domain === "humoq.de") {
-
 res.setHeader("Content-Type", "text/plain");
 res.write(`User-agent: *
 Allow: /
 
-Sitemap: https://humoq.de/sitemap.xml`);
+Sitemap: https://${req.headers['host']}/sitemap.xml`);
 res.end();
-
-    } else {
-
-res.setHeader("Content-Type", "text/plain");
-res.write(`User-agent: *
-Allow: /
-
-Sitemap: https://humoq.com/sitemap.xml`);
-res.end();
-
-    }
 
   }
 }
