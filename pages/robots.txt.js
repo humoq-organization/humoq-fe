f7762847991de2429524e7480a6f677485960467
karabatak <oktay.karabatak@gmail.com>
2021-12-05 21:31:45 +0300

    robots update url

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 class Robots extends React.Component {
+
   static async getInitialProps({ req, res }) {
 
 res.setHeader("Content-Type", "text/plain");
@@ -8,6 +9,7 @@ res.write(`User-agent: *
 Allow: /
 
 Sitemap: https://humoq.com/sitemap.xml`);
+
 res.end();
 
   }
