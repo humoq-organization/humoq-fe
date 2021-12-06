171e59185408aef4a699aa490291bd7879fb3f93
karabatak <oktay.karabatak@gmail.com>
2021-12-06 22:31:52 +0300

    update google site verification

--- components/Meta.js
+++ components/Meta.js
@@ -30,7 +30,7 @@ const Meta = ({title, keywords, description}) => {
             <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
             <link rel='icon' href='/favicon.ico' />
             <meta name="theme-color" content="#005fd7" />
-            <meta name="google-site-verification" content={(router.defaultLocale) === "de" ? "LjNr7JykcW_odEwrbWClpD9FWmak-sTuL9Bpi0smwJw" : "HC-OFVg-Qhmu0ZfReutqPBfqf5Rh3zivvoGZZ8wX4I0"} />
+            <meta name="google-site-verification" content="4zFZxhkX02v30Zq9u7CuXnrMgEpjkH0vFOYkpnAv75U" />
             <meta name="yandex-verification" content="0e6d0a8bb6555cf6" />
         </Head>
     )
