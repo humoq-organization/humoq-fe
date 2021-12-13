57c657362ded2381bc08b078b110858665d3dc1b
karabatak <oktay.karabatak@gmail.com>
2021-12-13 15:49:15 +0300

    update nextconfig test

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ module.exports = withPWA({
   i18n: {
     locales: ['en', 'de'],
     defaultLocale: 'en',
-    localeDetection: false,
     domains: [
         {
             domain: 'humoq.com',
